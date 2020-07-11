5606914392f44534fe4a1e68047e18fc998335ba
Paul D'Ambra <paul.dambra@gmail.com>
2020-07-11 11:52:34 +0100

    convert from copied code

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# get-to-1000
-a web page that teaches you how to get to 1000 with actual maths
+# Namey Numbers
+a web page that teaches you how to convert from a number as words to a number as numerals
 
+Numerals to Words conversion stolen from https://stackoverflow.com/a/30524915
 
 <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
--- index.js
+++ index.js
@@ -1,121 +1,150 @@
-    var diffToNearest = function(n, i) {
-        return (Math.ceil(i / n) * n) - i;
-    };
+const setupRow = (expectedNumeral, onCorrect) => {
+  const row = document.getElementById("numerals");
 
-    var setupRow = function(id, start, correctDiff, onCorrect) {
-        return function() {
-            var row = document.getElementById(id);
-            row.style.display = "";
+  const theInput = row.querySelectorAll("input")[0];
+  theInput.focus();
+  theInput.addEventListener('input', function (e) {
+    const answer = parseInt(this.value, 10);
+    const answerCorrect = answer === expectedNumeral;
+    row.querySelectorAll(".tick")[0].style.display = answerCorrect ? "" : "none";
+    row.querySelectorAll(".cross")[0].style.display = answerCorrect ? "none" : "";
 
-            if (row.querySelectorAll(".start").length > 0) {
-                row.querySelectorAll(".start")[0].innerHTML = start;
-            }
-
-            var theInput = row.querySelectorAll("input")[0];
-            theInput.focus();
-            theInput.addEventListener('input', function(e) {
-                var answer = parseInt(this.value, 10);
-                var answerCorrect = answer === correctDiff;
-                row.querySelectorAll(".tick")[0].style.display = answerCorrect ? "" : "none";
-                row.querySelectorAll(".cross")[0].style.display = answerCorrect ? "none" : "";
-
-                if (answerCorrect) {
-                    setTimeout(onCorrect, 500)
-                }
-            });
-        };
-    };
-
-    var start;
-    var tensDiff;
-    var nearestHundredStart;
-    var hundredsDiff;
-    var nearestThousandStart;
-    var thousandDiff;
-    var setupNearestThousand;
-    var setupNearestHundred;
-    var setupNearestTen;
-
-    var hideAll = function(selector) {
-        var nodes = document.querySelectorAll(selector);
-        for (var i = 0; i < nodes.length; i++) {
-            nodes[i].style.display = "none";
-        }
+    if (answerCorrect) {
+      setTimeout(onCorrect, 500)
     }
+  });
+};
 
-    var unsetAll = function() {
-        var nodes = document.querySelectorAll("input");
-        for (var i = 0; i < nodes.length; i++) {
-            nodes[i].value = "";
-        }
-    }
+const hide = selector => {
+  const nodes = document.querySelectorAll(selector);
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].style.display = "none";
+  }
+};
 
-    var ensureStartNumberDoesNotConfusinglyHaveZeroAsTheUnit = function(i) {
-        var asString = '' + i;
-        var onesInteger = asString[asString.length - 1];
-        return onesInteger === '0' ? ++i : i;
-    }
+const show = selector => {
+  const nodes = document.querySelectorAll(selector);
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].style.display = "";
+  }
+};
 
-    var nextImage;
-    var preloadNextCatUrl = function() {
+const resetAllInputs = () => {
+  const nodes = document.querySelectorAll("input");
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].value = "";
+  }
+};
 
-        var catGifUrl = "https://thecatapi.com/api/images/get?format=src&type=gif";
-        catGifUrl += "&cacheBuster=" + new Date().getTime();
+let nextImage;
+const preloadNextCatUrl = () => {
 
-        var i = new Image();
-        i.style.width = '100%';
-        i.onload = function() {
-          console.log(catGifUrl, 'preloaded!')
-          nextImage = i;
+  let catGifUrl = "https://thecatapi.com/api/images/get?format=src&type=gif";
+  catGifUrl += "&cacheBuster=" + new Date().getTime();
 
-        }
-                i.src = catGifUrl;
-    }
+  const i = new Image();
+  i.style.width = '100%';
+  i.onload = function () {
+    console.log(catGifUrl, 'preloaded!')
+    nextImage = i;
 
-    var addCatImage = function() {
-        var theGif = document.getElementById("cat-gif");
-        theGif.innerHTML = '';
-        theGif.appendChild(nextImage);
-    }
+  }
+  i.src = catGifUrl;
+};
 
-    var setupPage = function() {
-        preloadNextCatUrl();
+const addCatImage = () => {
+  const theGif = document.getElementById("cat-gif");
+  theGif.innerHTML = '';
+  theGif.appendChild(nextImage);
+};
 
-        start = (Math.floor(Math.random() * 1000) + 1);
-        start = ensureStartNumberDoesNotConfusinglyHaveZeroAsTheUnit(start);
+const arr = x => Array.from(x);
+const num = x => Number(x) || 0;
+const isEmpty = xs => xs.length === 0;
+const take = n => xs => xs.slice(0, n);
+const drop = n => xs => xs.slice(n);
+const reverse = xs => xs.slice(0).reverse();
+const comp = f => g => x => f(g(x));
+const not = x => !x;
+const chunk = n => xs =>
+  isEmpty(xs) ? [] : [take(n)(xs), ...chunk(n)(drop(n)(xs))];
 
-        tensDiff = diffToNearest(10, start);
-        nearestHundredStart = start + tensDiff;
-        hundredsDiff = diffToNearest(100, nearestHundredStart);
-        nearestThousandStart = nearestHundredStart + hundredsDiff;
-        thousandDiff = diffToNearest(1000, nearestThousandStart);
+const numToWords = n => {
 
-        hideAll(".row");
-        hideAll(".mark-holder span");
-        unsetAll();
+  let a = [
+    '', 'one', 'two', 'three', 'four',
+    'five', 'six', 'seven', 'eight', 'nine',
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+    'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+  ];
 
-        var initialRow = document.getElementById("initial-number");
-        initialRow.style.display = "";
-        initialRow.innerHTML = 'Your starting number is ' + start;
+  let b = [
+    '', '', 'twenty', 'thirty', 'forty',
+    'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+  ];
 
-        setupNearestThousand = setupRow("nearest-thousand", nearestThousandStart, thousandDiff, showDoneButton);
-        setupNearestHundred = setupRow("nearest-hundred", nearestHundredStart, hundredsDiff, setupNearestThousand);
-        setupNearestTen = setupRow("nearest-ten", start, tensDiff, setupNearestHundred);
+  let g = [
+    '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion',
+    'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion'
+  ];
 
-        setupNearestTen();
-    };
+  // this part is really nasty still
+  // it might edit this again later to show how Monoids could fix this up
+  let makeGroup = ([ones, tens, huns]) => {
+    return [
+      num(huns) === 0 ? '' : a[huns] + ' hundred ',
+      num(ones) === 0 ? b[tens] : b[tens] && b[tens] + '-' || '',
+      a[tens + ones] || a[ones]
+    ].join('');
+  };
 
-    document.getElementById("done").addEventListener("click", function() {
-        setupPage();
-    });
+  let thousand = (group, i) => group === '' ? group : `${group} ${g[i]}`;
 
-    var showDoneButton = function() {
-        hideAll(".row");
-        var buttonHolder = document.getElementById("done");
-        buttonHolder.querySelectorAll(".result")[0].innerHTML = start + " + " + tensDiff + " + " + hundredsDiff + " + " + thousandDiff + " = 1000"
-        buttonHolder.querySelectorAll("button")[0].innerHTML = "Awesome work! Again?";
-        addCatImage();
-        document.getElementById("done").style.display = "";
-    };
+  if (typeof n === 'number')
+    return numToWords(String(n));
+  else if (n === '0')
+    return 'zero';
+  else
+    return comp(chunk(3))(reverse)(arr(n))
+      .map(makeGroup)
+      .map(thousand)
+      .filter(comp(not)(isEmpty))
+      .reverse()
+      .join(' ');
+};
 
-    setupPage();
+const showDoneButton = (numerals, words) => function () {
+  hide("#numerals");
+  hide("#initial-number")
+  const buttonHolder = document.getElementById("done");
+  buttonHolder.querySelectorAll(".result")[0].innerHTML = words + " says " + numerals;
+  buttonHolder.querySelectorAll("button")[0].innerHTML = "Awesome work! Again?";
+  addCatImage();
+  show('#done')
+};
+
+const showWordToHuman = words => {
+  const initialRow = document.getElementById("initial-number");
+  initialRow.style.display = "";
+  initialRow.innerHTML = 'Your starting number is ' + words;
+};
+
+const setupPage = () => {
+  preloadNextCatUrl();
+
+  const numerals = (Math.floor(Math.random() * 999) + 1);
+  const words = numToWords(numerals);
+
+  show("#numerals");
+  hide(".mark-holder span");
+  hide("#done");
+  resetAllInputs();
+
+  showWordToHuman(words);
+
+  setupRow(numerals, showDoneButton(numerals, words));
+};
+
+document.getElementById("done").addEventListener("click", setupPage);
+
+setupPage();
