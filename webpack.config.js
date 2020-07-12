fc9b123a861b4eec968eeca02f04a073a1c54019
Paul D'Ambra <paul.dambra@gmail.com>
2020-07-12 11:00:50 +0100

    generate static page and js with webpack instead

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'template-index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.js' ],
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'docs'),
+    },
+    optimization: {
+        moduleIds: 'hashed',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'docs'),
+        compress: true,
+        port: 9000
+    }
+}
