d0accd14313fc5e62ab605eed840bbb64c3e7534
Paul D'Ambra <paul.dambra@gmail.com>
2020-07-12 11:09:40 +0100

    move numeral to word conversion to a module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const dom = require('./dom')
+const convert = require('./convert')
 
 const setupRow = (expectedNumeral, onCorrect) => {
   const row = document.getElementById("numerals");
@@ -39,61 +40,6 @@ const addCatImage = () => {
   theGif.appendChild(nextImage);
 };
 
-const arr = x => Array.from(x);
-const num = x => Number(x) || 0;
-const isEmpty = xs => xs.length === 0;
-const take = n => xs => xs.slice(0, n);
-const drop = n => xs => xs.slice(n);
-const reverse = xs => xs.slice(0).reverse();
-const comp = f => g => x => f(g(x));
-const not = x => !x;
-const chunk = n => xs =>
-  isEmpty(xs) ? [] : [take(n)(xs), ...chunk(n)(drop(n)(xs))];
-
-const numToWords = n => {
-
-  let a = [
-    '', 'one', 'two', 'three', 'four',
-    'five', 'six', 'seven', 'eight', 'nine',
-    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
-    'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
-  ];
-
-  let b = [
-    '', '', 'twenty', 'thirty', 'forty',
-    'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
-  ];
-
-  let g = [
-    '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion',
-    'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion'
-  ];
-
-  // this part is really nasty still
-  // it might edit this again later to show how Monoids could fix this up
-  let makeGroup = ([ones, tens, huns]) => {
-    return [
-      num(huns) === 0 ? '' : a[huns] + ' hundred ',
-      num(ones) === 0 ? b[tens] : b[tens] && b[tens] + '-' || '',
-      a[tens + ones] || a[ones]
-    ].join('');
-  };
-
-  let thousand = (group, i) => group === '' ? group : `${group} ${g[i]}`;
-
-  if (typeof n === 'number')
-    return numToWords(String(n));
-  else if (n === '0')
-    return 'zero';
-  else
-    return comp(chunk(3))(reverse)(arr(n))
-      .map(makeGroup)
-      .map(thousand)
-      .filter(comp(not)(isEmpty))
-      .reverse()
-      .join(' ');
-};
-
 const showDoneButton = (numerals, words) => function () {
   dom.hide("#numerals", document);
   dom.hide("#initial-number", document)
@@ -114,7 +60,7 @@ const setupPage = () => {
   preloadNextCatUrl();
 
   const numerals = (Math.floor(Math.random() * 999) + 1);
-  const words = numToWords(numerals);
+  const words = convert.numeralsToWords(numerals);
 
   dom.show("#numerals", document);
   dom.hide(".mark-holder span", document);
